81c1472eea965fe06339edcefd6610504d96b9e0
Javier E. <jaedns@gmail.com>
2024-11-22 21:19:46 -0300

    fix(gh-actions): small fixes (#3)

--- mango3-accounts/end2end/tests/login_page.spec.ts
+++ mango3-accounts/end2end/tests/login_page.spec.ts
@@ -1,7 +1,10 @@
 import { test, expect } from "@playwright/test";
+import { expectLoadToComplete } from "../../../js/shared_expects";
 
 test("login page has a heading text", async ({ page }) => {
     await page.goto("/login");
+
+    await expectLoadToComplete(page);
 
     await expect(page.locator("h2")).toHaveText("Login");
 });
--- mango3-home/end2end/tests/index_page.spec.ts
+++ mango3-home/end2end/tests/index_page.spec.ts
@@ -1,7 +1,10 @@
 import { test, expect } from "@playwright/test";
+import { expectLoadToComplete } from "../../../js/shared_expects";
 
 test("index page has a heading text", async ({ page }) => {
     await page.goto("/");
+
+    await expectLoadToComplete(page);
 
     await expect(page.locator("h2")).toHaveText("Welcome to Mango³ Dev!");
 });
