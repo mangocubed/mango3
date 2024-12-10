8c418d12aaf32220a697d9b8562536798962ae41
Javier E. <jaedns@gmail.com>
2024-12-10 00:16:02 -0300

    feat(posts): add attached images

--- mango3-leptos-utils/src/components/image_upload_field.rs
+++ mango3-leptos-utils/src/components/image_upload_field.rs
@@ -2,12 +2,11 @@ use leptos::either::EitherOf3;
 use leptos::ev::{Event, MouseEvent};
 use leptos::prelude::*;
 use leptos::text_prop::TextProp;
-use leptos_use::{use_clipboard, UseClipboardReturn};
 use wasm_bindgen::JsCast;
 use web_sys::{FormData, HtmlInputElement};
 
-use crate::i18n::{t_string, use_i18n};
-use crate::icons::{LinkOutlined, TrashOutlined};
+use crate::components::CopyableText;
+use crate::icons::TrashOutlined;
 use crate::models::BlobResp;
 use crate::server_functions::attempt_to_upload_file;
 
@@ -21,7 +20,6 @@ pub fn ImageUploadField(
     #[prop(default = 48)] width: i16,
     #[prop(into)] name: &'static str,
 ) -> impl IntoView {
-    let i18n = use_i18n();
     let upload_action = Action::new_local(|data: &FormData| attempt_to_upload_file(data.clone().into()));
     let upload_action_value = upload_action.value();
 
@@ -66,7 +64,6 @@ pub fn ImageUploadField(
                 <span class="label-text">{move || label.get()}</span>
             </label>
             {move || {
-                let UseClipboardReturn { copy, is_supported, .. } = use_clipboard();
                 if upload_action.pending().get() {
                     EitherOf3::A(view! { <span class="loading loading-spinner loading-lg"></span> })
                 } else if let Some(blob) = value.get() {
@@ -77,35 +74,7 @@ pub fn ImageUploadField(
                             <div class="flex gap-3">
                                 <img class="rounded" width=width height=height src=variant_url />
 
-                                <div class="grow input input-bordered flex items-center gap-2 pr-0">
-                                    <input class="grow" value=blob.url readonly />
-                                    <Show when=move || {
-                                        is_supported.get()
-                                    }>
-                                        {
-                                            let copy = copy.clone();
-                                            move || {
-                                                let copy = copy.clone();
-                                                let copy_url = move |event: MouseEvent| {
-                                                    event.prevent_default();
-                                                    if let Some(blob) = value.get() {
-                                                        copy(&blob.url);
-                                                    }
-                                                };
-
-                                                view! {
-                                                    <button
-                                                        class="btn btn-ghost"
-                                                        title=t_string!(i18n, shared.copy_url)
-                                                        on:click=copy_url
-                                                    >
-                                                        <LinkOutlined />
-                                                    </button>
-                                                }
-                                            }
-                                        }
-                                    </Show>
-                                </div>
+                                <CopyableText value=blob.url />
 
                                 <button class="btn" on:click=remove>
                                     <TrashOutlined />
@@ -115,6 +84,7 @@ pub fn ImageUploadField(
                     )
                 } else {
                     EitherOf3::C(
+
                         view! {
                             <input
                                 class="file-input file-input-bordered w-full"
