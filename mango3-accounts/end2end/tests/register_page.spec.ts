75cb2ddef775826647585faf3b81d732cfe2dc49
Javier E. <jaedns@gmail.com>
2024-12-13 15:55:20 -0300

    fix(ci): fix e2e test random error

--- mango3-accounts/end2end/tests/register_page.spec.ts
+++ mango3-accounts/end2end/tests/register_page.spec.ts
@@ -13,7 +13,7 @@ test("should register a new user", async ({ page }) => {
 
     await expect(page.locator("h2")).toHaveText("Register");
 
-    await page.getByLabel("Username").fill(faker.internet.username());
+    await page.getByLabel("Username").fill(faker.internet.username().substring(0, 16));
     await page.getByLabel("Email").fill(faker.internet.email());
     await page.getByLabel("Password").fill(faker.internet.password());
     await page.getByLabel("Full name").fill(faker.person.fullName());
