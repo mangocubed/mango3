02da58802cdc5d7d78fe8203ab07d4c2b12ab8f5
Javier E. <jaedns@gmail.com>
2025-01-10 15:16:50 -0300

    feat(handlebars): add support for handlebars on posts

--- mango3-leptos-utils/src/constants.rs
+++ mango3-leptos-utils/src/constants.rs
@@ -13,5 +13,5 @@ pub const KEY_PARAM_NAME: &str = "name";
 
 #[cfg(feature = "ssr")]
 pub(crate) static REGEX_HANDLEBARS_DECLARE: LazyLock<Regex> = LazyLock::new(|| {
-    Regex::new(r#"\{\{\s*declare\s+"(?<key>[[:word:]]+)"\s+((?<bool>|true|false)|(?<number>[0-9,\.-]+)|"(?<string>.*)"|(?<array>\[.*\])|(?<object>\{.*\}))\s*\}\}"#).unwrap()
+    Regex::new(r#"\{\{\s*declare\s+"(?<key>[[:word:]]+)"\s+((?<bool>|true|false)|(?<number>[0-9,\.-]+)|"(?<string>.*)"|(?<array>\[(?s:.)*\])|(?<object>\{(?s:.)*\}))\s*\}\}"#).unwrap()
 });
