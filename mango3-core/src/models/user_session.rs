c3c0a6bb58d657f5c4427ac87e254d6ec2d7f342
Javier E. <jaedns@gmail.com>
2025-02-07 15:07:45 -0300

    feat(users)!: store user session queries in a cache

--- mango3-core/src/models/user_session.rs
+++ mango3-core/src/models/user_session.rs
@@ -1,13 +1,21 @@
+use std::fmt::Display;
+
+use cached::proc_macro::io_cached;
+use cached::AsyncRedisCache;
+use futures::future;
+use serde::{Deserialize, Serialize};
 use sqlx::types::chrono::{DateTime, Utc};
 use sqlx::types::Uuid;
 use sqlx::{query, query_as};
 
 use crate::enums::MailerJobCommand;
 use crate::validator::ValidationErrors;
-use crate::CoreContext;
+use crate::{async_redis_cache, CoreContext};
 
-use super::User;
+use super::user_password_reset::GET_USER_PASSWORD_RESET_GET_BY_USER;
+use super::{AsyncRedisCacheTrait, User};
 
+#[derive(Clone, Deserialize, Serialize)]
 pub struct UserSession {
     pub id: Uuid,
     pub user_id: Uuid,
@@ -15,21 +23,42 @@ pub struct UserSession {
     pub updated_at: Option<DateTime<Utc>>,
 }
 
+impl Display for UserSession {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.id)
+    }
+}
+
 impl UserSession {
+    async fn all_by_user(core_context: &CoreContext, user: &User) -> Vec<UserSession> {
+        query_as!(Self, "SELECT * FROM user_sessions WHERE user_id = $1", user.id)
+            .fetch_all(&core_context.db_pool)
+            .await
+            .unwrap_or_default()
+    }
+
     pub async fn delete(&self, core_context: &CoreContext) -> Result<(), ValidationErrors> {
         query!("DELETE FROM user_sessions WHERE id = $1", self.id)
             .execute(&core_context.db_pool)
             .await
             .map(|_| ())
-            .map_err(|_| ValidationErrors::default())
+            .map_err(|_| ValidationErrors::default())?;
+
+        GET_USER_PASSWORD_RESET_GET_BY_USER.cache_remove(&self.id).await;
+
+        Ok(())
     }
 
     pub async fn delete_all(core_context: &CoreContext, user: &User) -> Result<(), ValidationErrors> {
-        query!("DELETE FROM user_sessions WHERE user_id = $1", user.id)
-            .execute(&core_context.db_pool)
-            .await
-            .map(|_| ())
-            .map_err(|_| ValidationErrors::default())
+        future::join_all(
+            Self::all_by_user(core_context, user)
+                .await
+                .iter()
+                .map(|user_session| user_session.delete(core_context)),
+        )
+        .await;
+
+        Ok(())
     }
 
     pub async fn exists(core_context: &CoreContext, id: Uuid) -> bool {
@@ -40,9 +69,7 @@ impl UserSession {
     }
 
     pub async fn get_by_id(core_context: &CoreContext, id: Uuid) -> Result<Self, sqlx::Error> {
-        query_as!(Self, "SELECT * FROM user_sessions WHERE id = $1 LIMIT 1", id)
-            .fetch_one(&core_context.db_pool)
-            .await
+        get_user_session_by_id(core_context, id).await
     }
 
     pub async fn insert(core_context: &CoreContext, user: &User) -> Result<Self, ValidationErrors> {
@@ -67,4 +94,16 @@ impl UserSession {
     pub async fn user(&self, core_context: &CoreContext) -> Result<User, sqlx::Error> {
         User::get_by_id(core_context, self.user_id).await
     }
+}
+
+#[io_cached(
+    map_error = r##"|_| sqlx::Error::RowNotFound"##,
+    convert = r#"{ id }"#,
+    ty = "AsyncRedisCache<Uuid, UserSession>",
+    create = r##" { async_redis_cache("get_user_session_by_id").await } "##
+)]
+pub(crate) async fn get_user_session_by_id(core_context: &CoreContext, id: Uuid) -> Result<UserSession, sqlx::Error> {
+    query_as!(UserSession, "SELECT * FROM user_sessions WHERE id = $1 LIMIT 1", id)
+        .fetch_one(&core_context.db_pool)
+        .await
 }
