bd59c18d6e2741b3437dc2c8d3c322f1249e039d
Javier E. <jaedns@gmail.com>
2025-04-12 21:02:10 -0300

    refact(post-reactions): add lifetime to model

--- packs/mango3-core/src/commands/post_reaction_commands.rs
+++ packs/mango3-core/src/commands/post_reaction_commands.rs
@@ -1,25 +1,25 @@
 use crate::models::*;
-use crate::CoreContext;
 
 #[cfg(feature = "delete-post-reaction")]
-pub async fn delete_post_reaction(core_context: &CoreContext, post_reaction: &PostReaction) -> crate::utils::MutResult {
+pub async fn delete_post_reaction(post_reaction: &PostReaction<'_>) -> crate::utils::MutResult {
+    let db_pool = crate::db_pool().await;
+
     sqlx::query!("DELETE FROM post_reactions WHERE id = $1", post_reaction.id)
-        .execute(&core_context.db_pool)
+        .execute(db_pool)
         .await?;
 
     crate::mut_success!()
 }
 
 #[cfg(feature = "get-post-reaction-emojis-count")]
-pub async fn get_post_reaction_emojis_count(
-    core_context: &CoreContext,
-    post: &Post,
-) -> sqlx::Result<Vec<(String, i64)>> {
+pub async fn get_post_reaction_emojis_count(post: &Post) -> sqlx::Result<Vec<(String, i64)>> {
+    let db_pool = crate::db_pool().await;
+
     sqlx::query!(
         "SELECT emoji, COUNT(*) as count FROM post_reactions WHERE post_id = $1 GROUP BY emoji ORDER BY count DESC",
         post.id, // $1
     )
-    .fetch_all(&core_context.db_pool)
+    .fetch_all(db_pool)
     .await
     .map(|records| {
         records
@@ -30,40 +30,40 @@ pub async fn get_post_reaction_emojis_count(
 }
 
 #[cfg(feature = "get-post-reaction-by-post-and-user")]
-pub async fn get_post_reaction_by_post_and_user(
-    core_context: &CoreContext,
-    post: &Post,
-    user: &User,
-) -> sqlx::Result<PostReaction> {
+pub async fn get_post_reaction_by_post_and_user<'a>(post: &Post, user: &User) -> sqlx::Result<PostReaction<'a>> {
+    let db_pool = crate::db_pool().await;
+
     sqlx::query_as!(
         PostReaction,
         "SELECT * FROM post_reactions WHERE post_id = $1 AND user_id = $2 LIMIT 1",
         post.id, // $1
         user.id, // $2
     )
-    .fetch_one(&core_context.db_pool)
+    .fetch_one(db_pool)
     .await
 }
 
 #[cfg(feature = "get-post-reactions-count")]
-pub async fn get_post_reactions_count(core_context: &CoreContext, post: &Post) -> i64 {
+pub async fn get_post_reactions_count(post: &Post) -> i64 {
+    let db_pool = crate::db_pool().await;
+
     sqlx::query!(
         "SELECT COUNT(*) FROM post_reactions WHERE post_id = $1 LIMIT 1",
         post.id, // $1
     )
-    .fetch_one(&core_context.db_pool)
+    .fetch_one(db_pool)
     .await
     .map(|record| record.count.unwrap_or_default())
     .unwrap_or_default()
 }
 
 #[cfg(feature = "insert-or-update-post-reaction")]
-pub async fn insert_or_update_post_reaction(
-    core_context: &CoreContext,
+pub async fn insert_or_update_post_reaction<'a>(
     post: &Post,
     user: &User,
     emoji: &str,
-) -> crate::utils::MutResult<PostReaction> {
+) -> crate::utils::MutResult<PostReaction<'a>> {
+    let db_pool = crate::db_pool().await;
     let mut validator = crate::validator!();
 
     validator.custom_validation(crate::enums::Input::Emoji, crate::enums::InputError::IsInvalid, &|| {
@@ -74,14 +74,14 @@ pub async fn insert_or_update_post_reaction(
         return crate::mut_error!(validator.errors);
     }
 
-    if let Ok(reaction) = get_post_reaction_by_post_and_user(core_context, post, user).await {
+    if let Ok(reaction) = get_post_reaction_by_post_and_user(post, user).await {
         let result = sqlx::query_as!(
             PostReaction,
             "UPDATE post_reactions SET emoji = $1 WHERE id = $2 RETURNING *",
             emoji,       // $1
             reaction.id, // $2
         )
-        .fetch_one(&core_context.db_pool)
+        .fetch_one(db_pool)
         .await;
 
         return crate::mut_result!(result);
@@ -94,7 +94,7 @@ pub async fn insert_or_update_post_reaction(
         user.id, // $2
         emoji,   // $3
     )
-    .fetch_one(&core_context.db_pool)
+    .fetch_one(db_pool)
     .await;
 
     crate::mut_result!(result)
@@ -111,7 +111,7 @@ mod tests {
         let core_context = setup_core_context().await;
         let post = insert_test_post(&core_context, None, None).await;
 
-        let count = get_post_reactions_count(&core_context, &post).await;
+        let count = get_post_reactions_count(&post).await;
 
         assert_eq!(count, 0);
     }
@@ -122,7 +122,7 @@ mod tests {
         let post = insert_test_post(&core_context, None, None).await;
         let user = insert_test_user(&core_context).await;
 
-        let result = get_post_reaction_by_post_and_user(&core_context, &post, &user).await;
+        let result = get_post_reaction_by_post_and_user(&post, &user).await;
 
         assert!(result.is_err());
     }
@@ -133,7 +133,7 @@ mod tests {
         let post = insert_test_post(&core_context, None, None).await;
         let user = insert_test_user(&core_context).await;
 
-        let result = insert_or_update_post_reaction(&core_context, &post, &user, "🙂").await;
+        let result = insert_or_update_post_reaction(&post, &user, "🙂").await;
 
         assert!(result.is_ok());
     }
--- packs/mango3-core/src/models/post_reaction.rs
+++ packs/mango3-core/src/models/post_reaction.rs
@@ -1,11 +1,13 @@
+use std::borrow::Cow;
+
 use chrono::{DateTime, Utc};
 use uuid::Uuid;
 
-pub struct PostReaction {
+pub struct PostReaction<'a> {
     pub id: Uuid,
     pub post_id: Uuid,
     pub user_id: Uuid,
-    pub emoji: String,
+    pub emoji: Cow<'a, str>,
     pub created_at: DateTime<Utc>,
     pub updated_at: Option<DateTime<Utc>>,
 }
